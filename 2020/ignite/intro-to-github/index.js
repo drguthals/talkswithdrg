bb75a390871168577cf54e91c0bb8a3c45f21aa4
Sarah Guthals <sarah@guthals.com>
2020-09-21 16:51:12 -0700

    trying actions again

--- 2020/ignite/intro-to-github/index.js
+++ 2020/ignite/intro-to-github/index.js
@@ -1,3 +1,5 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
 const fs = require( "fs" );
 const path = require( "path" );
 const md = require( "markdown-it" )({
@@ -5,38 +7,25 @@ const md = require( "markdown-it" )({
     typographer: true
 }).enable([ 'link' ]);
 
-console.log( "Scanning Social Shareboard entries..." );
-let shareboardFull = fs.readdirSync( path.join( __dirname, "notes" ) )
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+
+  console.log( "Scanning Social Shareboard entries..." );
+  let shareboardFull = fs.readdirSync( path.join( __dirname, "notes" ) )
     .filter( file => path.extname( file ).toLowerCase() === ".md" )
     .filter( file => file !== "sample.md" )
     .map( file => fs.readFileSync( path.join( __dirname, "notes", file ) ) )
     .join( "\n---\n" );
 
-// Save the Markdown
-fs.writeFileSync( "ignite-shareboard.md", shareboardFull );
-
-// Save the HTML
-let shareboard = md.render( fs.readFileSync( "ignite-shareboard.md" ).toString() );
-let page = `<!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap/3.2.0/css/bootstrap.css">
-        <title>Social Shareboard for MS Ignite 2020!</title>
-    </head>
-    <body>
-        <div class="jumbotron">
-            <h1>#MSIgnite 2020</h1>
-            <p>This is a Social Shareboard with messages submitted by participants during the Microsoft Ignite 2020 event between September 22-24, 2020</p>
-        </div>
-        <br/>
-        <div class="container">
-        ${shareboard}
-        </div>
-        <script src="https://cdn.jsdelivr.net/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    </body>
-</html>`;
-fs.writeFileSync( "index.html", page );
-
-const Web = require( "webwebweb" ).Run( 8000 );
+  // Save the Markdown
+  fs.writeFileSync( "ignite-shareboard.md", shareboardFull );
+} catch (error) {
+  core.setFailed(error.message);
+}
