a8c4ac2391cea1e9cc9f8179babd855d99d34cb2
Sarah Guthals <sarah@guthals.com>
2020-09-21 21:13:44 -0700

    trying octokit again

--- 2020/ignite/intro-to-github/index.js
+++ 2020/ignite/intro-to-github/index.js
@@ -2,7 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require( "fs" );
 const path = require( "path" );
-
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit();
 
 async function run() {
   try {
@@ -16,10 +17,20 @@ async function run() {
   
     console.log( "Shareboard contents:\n");
     console.log(shareboardFull);
+
+    console.log("Attempting to update file with octokit");
   
-    console.log("Attempting to update the contents with the notes folder")
+    await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+      owner: 'sguthals',
+      repo: 'talkswithdrg',
+      path: '2020/ignite/intro-to-github/ignite-shareboard.md',
+      message: 'updating with a new content',
+      content: shareboardFull
+    })
+
+    //console.log("Attempting to update the contents with the notes folder")
     // Save the Markdown
-    fs.writeFileSync( "ignite-shareboard.md", shareboardFull );
+    //fs.writeFileSync( "ignite-shareboard.md", shareboardFull );
   
     //console.log("Ignite shareboard now has:\n");
     //console.log(fs.readdirSync( path.join( __dirname, "ignite-shareboard.md") ));
